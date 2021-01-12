adf66affc6c646ad3221e5ca9d51d7ad8ebaa94e
t4h4 <thyasinerkan@gmail.com>
2021-01-12 23:43:34 +0300

    register non google sign up

---
+++ src/services/auth-service.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:4000/";
+
+class AuthService {
+  register(username, email, password) {
+    return axios.post(API_URL + "users", {
+      username,
+      email,
+      password,
+    });
+  }
+}
+
+export default new AuthService();
